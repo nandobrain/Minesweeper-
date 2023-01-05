939e91cefb9497177326271ef3408f50f17a3d44
nandobrain <prf.arturo@gmail.com>
2023-01-05 15:43:47 -0800

    logic for mine geneneration

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 
+let mineAmount = 10
+let minesLocation = [] 
+
+
+// logic for mine placement
+let initializeMines = (mineAmount) => {
+  for (i = 0; i < mineAmount; i++) {
+    let mineNumbers = Math.floor(Math.random() * 100) +1;
+    minesLocation.push(mineNumbers)
+    }
+}
+
+initializeMines(mineAmount)
+
+console.log(minesLocation)
+
+//game function
+function startGame() {
+   
+
+
+}
+// get start button to function
+let startButton = document.getElementById("play")
+startButton.addEventListener("click", startGame)
+
+
+
